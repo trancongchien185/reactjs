7128a19682cfe041734f4468f90ccc8df7cb9823
trancongchien <trancongchien185@gmail.com>
2022-10-27 21:12:22 +0700

    AJAX with Axios

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@
 //  Import the React Library
 import React from "react";
 import  ReactDOM  from "react-dom";
+import axios from 'axios';
 import ImageList from "./companents/image_list";
 
 // Creact a comoment
@@ -16,6 +17,7 @@ const App = () => {
  
 //  Render this component th the screen
 Meteor.startup(() => {
-
+    ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get('http://api.imgur.com/3/gallery/hot/viral/0')
+        .then(response => console.log(response));
 })
-ReactDOM.render(<App />, document.querySelector('.container'));
